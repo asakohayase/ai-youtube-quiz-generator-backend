94b3442d033b08efd92b88bd836959b39b77e046
Asako Hayase <chipmunk.kck@gmail.com>
2024-08-25 00:04:58 -0700

    change middleware

--- middleware.py
+++ middleware.py
@@ -4,10 +4,12 @@ import os
 
 
 def add_middleware(app: FastAPI) -> None:
-    frontend_url = os.getenv("FRONTEND_URL", "http://localhost:3000")
+    # frontend_url = os.getenv("FRONTEND_URL", "http://localhost:3000")
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=[frontend_url],
+        allow_origins=[
+            "https://aiyoutubequizgenerator-asakohayase-asakohayases-projects.vercel.app/"
+        ],
         allow_methods=["*"],
         allow_headers=["*"],
     )
