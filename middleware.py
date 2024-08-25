bd64c7f31008ececd75ec8ec615a8d449be46b49
Asako Hayase <chipmunk.kck@gmail.com>
2024-08-25 00:08:44 -0700

    diable cors

--- middleware.py
+++ middleware.py
@@ -7,9 +7,7 @@ def add_middleware(app: FastAPI) -> None:
     # frontend_url = os.getenv("FRONTEND_URL", "http://localhost:3000")
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=[
-            "https://aiyoutubequizgenerator-asakohayase-asakohayases-projects.vercel.app/"
-        ],
+        allow_origins=["*"],
         allow_methods=["*"],
         allow_headers=["*"],
     )
