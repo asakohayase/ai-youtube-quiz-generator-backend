27600583f2e71a074cb3917ec46d3fe620c35df5
Asako Hayase <chipmunk.kck@gmail.com>
2024-08-25 00:11:13 -0700

    change the cors back

--- middleware.py
+++ middleware.py
@@ -7,7 +7,7 @@ def add_middleware(app: FastAPI) -> None:
     # frontend_url = os.getenv("FRONTEND_URL", "http://localhost:3000")
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],
+        allow_origins=["frontend_url"],
         allow_methods=["*"],
         allow_headers=["*"],
     )
